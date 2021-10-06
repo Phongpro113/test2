3fa8d73cf0b1850e222d6954b12775eb302d4eef
Your Name <you@example.com>
2021-10-06 11:07:35 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Chào mừng các bạn đến đây</h1>
+    <h2 style="color: violet;"> Tớ yêu các bạn</h2>
+    <p>Nhà nghèo, không có tiền đi học phải vào học chui để có cơ hội tiếp thu thể hiện kiến thức</p>
+    <p>tớ chỉ phát biểu kiến thức, mong các bạn cho tớ đc học</p>
+</body>
+</html>
