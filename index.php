9c18a6c5635b98e37943c7ad72cc2992cd8c7b5e
Your Name <you@example.com>
2021-10-06 14:42:29 +0700

    ok1

--- index.php
+++ index.php
@@ -7,9 +7,32 @@
     <title>Document</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Chào mừng các bạn đến đây</h1>
+    <h1 style="text-align: center;">Chào mừng các bạn đến </h1>
+    <h1 style="text-align: center; color: blueviolet;">Ngôi nhà của HKan</h1>
     <h2 style="color: violet;"> Tớ yêu các bạn</h2>
-    <p>Nhà nghèo, không có tiền đi học phải vào học chui để có cơ hội tiếp thu thể hiện kiến thức</p>
-    <p>tớ chỉ phát biểu kiến thức, mong các bạn cho tớ đc học</p>
+    <br>
+    <p>Chuyện của HKan <br></p>
+    <p>Năm lớp 6 tớ bỏ học theo Idol của tớ đi làm coder, Tớ không được đi học bài bản, nhưng tớ cũng có đọc SGK</p>
+     <p>tớ chỉ muốn được vào lớp học biết thêm kiến thức </p> 
+    <p style="color: red;">mong các bạn cho tớ đc học</p>
+    <p>Tớ không phá gì cả</p>
+    <h1 style="color: violet;">"Yêu các bạn - Chúng ta cùng cố gắng<3"</h1>
+   
+    <p><br></p>
+    <img src="https://i.chungta.vn/2013/06/14/450laptrinh-336356-1413003315.jpg" alt="">
+    <p>Mình xin chia sẻ chuyện ngành code của mình <br><br><br></p>
+    <p>Thằng anh mình học bách khoa cơ khí, sinh năm 96. Tự mày mò học code rồi đi làm remote cho công ty Mỹ 2 năm nay. <br>
+         Mỗi tối online 3-4 giờ là xong việc. Lương tháng 3k6. Nhưng thu nhập chính vẫn là từ nhận các project bên ngoài làm thêm. <br>
+        Tuần làm 2,3 cái nhẹ nhàng 9,10k tiền tươi thóc thật không phải đóng thuế. <br>
+         Làm gần được 3 năm mà nhà xe nó đã mua đủ cả. Nghĩ mà thèm.<br>
+        
+         Gái gú thì cứ nghe nó bảo làm CNTT thì chảy nước. <br>
+         Có bé kia dân du học sinh Úc, về được cô chị giới thiệu làm ngân hàng VCB. <br>
+         Thế nào thằng ấy đi mở thẻ tín dụng gặp phải thế là hốt được cả chị lẫn em. 3 đứa nó sống chung một căn hộ cao cấp. <br>
+         Nhà con bé kia biết chuyện ban đầu phản đối sau biết thằng đấy học IT thì đổi thái độ, <br>
+         cách ba bữa hỏi thăm, năm bữa tặng quà lấy long, luôn giục cưới kẻo lỡ kèo ngon.<br>
+         Dạo này dịch bệnh nên trì hoãn sang năm sau làm lễ <br><br><br>
+        </p>
+        <p style="color: brown;"> Bây giờ là thời đại công nghệ, Mong các bạn cố học tốt tin, chú ý nghe giảng những gì cô dạy, để tương lai tươi sáng</p>
 </body>
 </html>
